2d19c52b2111a904df10b11cfa1e5c1626db400a
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-07-19 23:27:02 +0700

    outsourcing validation logic

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Input from "./Input.jsx";
+import { hasMinLength, isEmail, isNotEmpty } from "../util/validation.js";
 
 export default function Login() {
   //OLD
@@ -17,9 +18,13 @@ export default function Login() {
     password: false,
   });
 
-  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
+  const emailIsInvalid =
+    didEdit.email &&
+    !isEmail(enteredValues.email) &&
+    !isNotEmpty(enteredValues.email);
+
   const passwordIsInvalid =
-    didEdit.password && enteredValues.password.trim().length < 6;
+    didEdit.password && !hasMinLength(enteredValues.password, 6);
 
   function handleSubmit(event) {
     event.preventDefault();
